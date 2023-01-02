0c2ba34a1d1f38a798beba6ceb5fd977c49f1170
aliesia-zahorui <aliesia@ukr.net>
2023-01-02 16:48:38 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { GlobalStyle } from './GlobalStyle';
@@ -12,145 +12,105 @@ import Loader from './Loader';
 import PendingErrorMessage from './PendingErrorMessage';
 import fetchPhotosAPI from '../services/photos-api';
 
-export class App extends Component {
-  static propTypes = {
-    photos: PropTypes.array,
-    totalPages: PropTypes.number,
-    searchQuery: PropTypes.string,
-    showModal: PropTypes.bool,
-    error: PropTypes.string,
-    status: PropTypes.string,
-    page: PropTypes.number,
-    largeImageURL: PropTypes.string,
-    tags: PropTypes.string,
-  };
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [photos, setPhotos] = useState(null);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [tags, setTags] = useState('');
 
-  state = {
-    searchQuery: '',
-    photos: null,
-    page: 1,
-    error: null,
-    status: 'idle',
-    showModal: false,
-    largeImageURL: '',
-    tags: '',
-  };
+  useEffect(() => {
+    if (!searchQuery) {
+      console.log('dont work in first render');
+      return;
+    }
 
-  componentDidUpdate(prevProps, prevState) {
-    //попередній та поточний запити
-    const prevQuery = prevState.searchQuery;
-    const nextQuery = this.state.searchQuery;
-    //попередня та поточна сторінка
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-    const firstPage = 1;
+    //задати початковий стейт
+    setPage(1);
+    setPhotos(null);
+    setStatus('pending');
+    setError(null);
 
-    //якщо запит змінився
-    if (nextQuery !== prevQuery) {
-      //задати початковий стейт
-      this.setState({
-        page: firstPage,
-        photos: null,
-        status: 'pending',
-        error: null,
+    //запит до API з новим запитом
+    fetchPhotosAPI(searchQuery, 1)
+      .then(photos => {
+        setPhotos(photos);
+        setStatus('resolved');
+      })
+      //обробка звичайної помилки
+      .catch(error => {
+        setStatus('regected');
+        setError(error);
       });
-      //запит до API з новим запитом
-      fetchPhotosAPI(nextQuery, firstPage)
-        .then(photos => {
-          // // обробка помилки (поки не працює)
-          // this.setState({
-          //   status: 'rejected',
-          // });
-          // if (photos.length === 0) {
-          //   return Promise.reject(
-          //     new Error(`Cant find anithing by your query ${nextQuery}`)
-          //   );
-          // }
-          //записати в стейт нові значення (відповідь сервера, зміна статусу)
-          this.setState({
-            photos,
-            status: 'resolved',
-          });
-        })
-        //обробка звичайної помилки
-        .catch(error =>
-          this.setState({
-            error,
-            status: 'regected',
-          })
-        );
-    }
+  }, [searchQuery]);
 
-    //якщо сторінка змінилась
-    if (nextPage !== firstPage && nextPage !== prevPage) {
-      fetchPhotosAPI(nextQuery, nextPage)
-        .then(newPhotos => {
-          //записати в стейт нові значення (відповід сервера + запис нового запита, зміна статусу)
-          this.setState({
-            photos: [...this.state.photos, ...newPhotos],
-            status: 'resolved',
-          });
-        })
-        //обробка звичайної помилки
-        .catch(error =>
-          this.setState({
-            error,
-            status: 'regected',
-          })
-        );
-    }
-  }
+  useEffect(() => {
+    fetchPhotosAPI(searchQuery, page).then(newPhotos => {
+      //записати в стейт нові значення (відповід сервера + запис нового запита, зміна статусу)
+      setPhotos([...photos, ...newPhotos]);
+      setStatus('resolved');
+    });
+    //обробка звичайної помилки
+    // .catch(error => {
+    //   setStatus('regected');
+    //   setError(error);
+    // });
+  }, [page]);
 
-  //відкриття-закриття модалного вікна через стейт
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  //заменить на просто подстановку setSearchQuery(searchQuery)
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
   };
 
-  //отримання запиту користувача з форми
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery });
+  const toggleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
   };
 
-  //відкриття модального вікна у елементах списку з передачею великоформатного зображення
-  onOpenModal = (url, tags) => {
-    this.setState({ largeImageURL: url, tags });
+  const onOpenModal = (url, tags) => {
+    setLargeImageURL(url);
+    setTags(tags);
 
-    this.toggleModal();
+    toggleModal();
   };
 
-  //
-  onLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const onLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { showModal, photos, error, status, largeImageURL, tags } =
-      this.state;
+  return (
+    <Box display="grid" gridTemplateColumns="1fr" gridGap="16px" pb="24px">
+      <Searchbar onSearch={handleFormSubmit} />
 
-    return (
-      <Box display="grid" gridTemplateColumns="1fr" gridGap="16px" pb="24px">
-        <Searchbar onSearch={this.handleFormSubmit} />
+      {status === 'idle' && <div>Enter your photo query</div>}
 
-        {status === 'idle' && <div>Enter your photo query</div>}
+      {status === 'resolved' && (
+        <>
+          <ImageGallery photos={photos} onOpenModal={onOpenModal} />
+          {photos.length === 12 && <Button onLoadMore={onLoadMore} />}
+        </>
+      )}
+      {status === 'pending' && <Loader />}
+      {status === 'rejected' && <PendingErrorMessage message={error.message} />}
 
-        {status === 'resolved' && (
-          <>
-            <ImageGallery photos={photos} onOpenModal={this.onOpenModal} />
-            {photos.length === 12 && <Button onLoadMore={this.onLoadMore} />}
-          </>
-        )}
-        {status === 'pending' && <Loader />}
-        {status === 'rejected' && (
-          <PendingErrorMessage message={error.message} />
-        )}
+      {showModal && (
+        <Modal onClose={toggleModal} src={largeImageURL} alt={tags} />
+      )}
+      <GlobalStyle />
+    </Box>
+  );
+};
 
-        {showModal && (
-          <Modal onClose={this.toggleModal} src={largeImageURL} alt={tags} />
-        )}
-        <GlobalStyle />
-      </Box>
-    );
-  }
-}
+App.propTypes = {
+  photos: PropTypes.array,
+  totalPages: PropTypes.number,
+  searchQuery: PropTypes.string,
+  showModal: PropTypes.bool,
+  error: PropTypes.string,
+  status: PropTypes.string,
+  page: PropTypes.number,
+  largeImageURL: PropTypes.string,
+  tags: PropTypes.string,
+};
